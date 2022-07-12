ec6b911bc96aa5b0c76b94379976939da9909d3b
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-07-12 21:21:00 +0530

    Form Data Used

--- src/app/App.js
+++ src/app/App.js
@@ -5,14 +5,19 @@ import Login from "../pages/session/Login";
 import {Routes, Route} from "react-router-dom";
 import HomePage from "../pages/Home";
 import NotFound from "../pages/session/NotFound";
+import Posts from "../pages/Posts";
+import FlexBoxLayout from "../pages/Layouts/FlexBox";
+import GridLayout from "../pages/Layouts/Grid";
+
 
 function App() {
   return (
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
+          <Route exact path='posts' element={<Posts/>}/>
         <Route path='login' element={<Login/>}/>
-        {/*<Route path='flex-layout' element={<FlexBoxLayout/>}/>*/}
-        {/*<Route path='grid-layout' element={<GridLayout/>}/>*/}
+        <Route path='flex-layout' element={<FlexBoxLayout/>}/>
+        <Route path='grid-layout' element={<GridLayout/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
   );
